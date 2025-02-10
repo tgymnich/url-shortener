e5a261578d0d26a2f641914d90520b89119798d6
Tim Gymnich <tgymnich@icloud.com>
2025-02-10 21:50:34 +0100

    minify only for prod

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  root: 'vue-app',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    minify: true,
-  },
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ]
-});
+// export default defineConfig({
+
+// });
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  return {
+    root: 'vue-app',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      minify: mode == 'production',
+    },
+    plugins: [
+      vue(),
+      tailwindcss(),
+    ]
+  }
+})
